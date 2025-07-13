43f46f93f70b492305fe6267790378025cacad86
Alex <kov.alex0602@gmail.com>
2025-07-13 16:35:47 +0300

    chore: add libvaxis for tui

--- build.zig
+++ build.zig
@@ -10,6 +10,13 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    // add vaxis dependency to module
+    const vaxis = b.dependency("vaxis", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe_mod.addImport("vaxis", vaxis.module("vaxis"));
+
     const exe = b.addExecutable(.{
         .name = "habitflow",
         .root_module = exe_mod,
